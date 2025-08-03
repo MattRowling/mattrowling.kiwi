e5759bbe19b3123e7c060b74edf36618d55e9012
MattRowling <matt@rowling.co.nz>
2025-08-03 15:11:17 +1000

    set title font

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <div className="bg-yellow-50 min-h-screen min-w-screen">
-        <h1 className="font-bold text-black p-5">mattrowling.🥝</h1>
+        <h1 className="font-sans font-bold text-black p-5">mattrowling.🥝</h1>
       </div>
     </>
   )
