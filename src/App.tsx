acf4abffbd15ea665640fb6ece18d711fcd2b75d
MattRowling <matt@rowling.co.nz>
2026-02-15 11:25:58 +1100

    add full fixtures table

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import "./App.css";
+import Fixtures from "./sports/Fixtures";
 
 function App() {
   return (
-    <>
-      <div className="bg-yellow-50 min-h-screen min-w-screen">
-        <h1 className="font-sans font-bold text-black p-5">
-          mattrowling.
-          <a href="https://www.hello.kiwi/why-dotkiwi" target="_blank">
-            🥝
-          </a>
-        </h1>
-      </div>
-    </>
+    <div className="bg-yellow-50 min-h-screen w-full overflow-hidden p-10">
+      <h1 className="font-sans font-bold text-black mb-10">
+        mattrowling.
+        <a href="https://www.hello.kiwi/why-dotkiwi" target="_blank">
+          🥝
+        </a>
+      </h1>
+      <Fixtures />
+    </div>
   );
 }
 
