ea966fbb0e734f2e52d7d50fb345567730b0f981
MattRowling <matt@rowling.co.nz>
2026-02-15 12:38:41 +1100

    add sports emojis and style times

--- src/sports/Fixtures.tsx
+++ src/sports/Fixtures.tsx
@@ -8,7 +8,38 @@ import {
   Paper,
 } from "@mui/material";
 
-import FIXTURES from "./data/FixturesData";
+import FixturesData from "./data/FixturesData";
+import FootballSvg from "./icons/soccer-ball-noto.svg";
+import RugbySvg from "./icons/rugby-football-noto.svg";
+import CricketSvg from "./icons/cricket-game-noto.svg";
+
+const getSvgForSport = (sport: string) => {
+  const style = { width: 24, height: 24 };
+  switch (sport.toLowerCase()) {
+    case "football":
+      return <img src={FootballSvg} style={style} />;
+    case "rugby":
+      return <img src={RugbySvg} style={style} />;
+    case "cricket":
+      return <img src={CricketSvg} style={style} />;
+  }
+  return null;
+};
+
+const getStartFromTimestamp = (timestamp: number) => {
+  // TODO: display year if not current year
+
+  const date = new Date(timestamp * 1000);
+  return date.toLocaleString("en-AU", {
+    weekday: "short",
+    month: "short",
+    day: "numeric",
+    hour: "numeric",
+    minute: "numeric",
+  });
+};
+
+const sortedFixtures = FixturesData.sort((a, b) => a.timestamp - b.timestamp);
 
 function Fixtures() {
   return (
@@ -22,29 +53,27 @@ function Fixtures() {
             }}
           >
             <TableRow>
-              <TableCell>Sport</TableCell>
+              <TableCell></TableCell>
               <TableCell>Home</TableCell>
               <TableCell>Away</TableCell>
-              <TableCell>Time (UTC)</TableCell>
-              <TableCell>Date</TableCell>
+              <TableCell>Start</TableCell>
               <TableCell>Venue</TableCell>
               <TableCell>City</TableCell>
               <TableCell>Competition</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {FIXTURES.map((row) => (
+            {sortedFixtures.map((row) => (
               <TableRow
                 key={row.id}
                 sx={{
                   "&:last-child td": { border: 0 },
                 }}
               >
-                <TableCell>{row.sport}</TableCell>
+                <TableCell>{getSvgForSport(row.sport)}</TableCell>
                 <TableCell>{row.home}</TableCell>
                 <TableCell>{row.away}</TableCell>
-                <TableCell>{row.timeUTC}</TableCell>
-                <TableCell>{row.date}</TableCell>
+                <TableCell>{getStartFromTimestamp(row.timestamp)}</TableCell>
                 <TableCell>{row.venue}</TableCell>
                 <TableCell>{row.city}</TableCell>
                 <TableCell>{row.competition}</TableCell>
